b03fb0bdd94a13d92bccd44066c06bd36c76e768
truong.nn <truong.nn@amanotes.com>
2023-03-07 17:20:51 +0700

    add  UI

--- auction-system-fe/src/pages/CreateItem/CreateItem.tsx
+++ auction-system-fe/src/pages/CreateItem/CreateItem.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-function CreateItem() {
+export default function CreateItem() {
+  const handleSubmit = (event: {
+    preventDefault: () => void;
+    currentTarget: HTMLFormElement | undefined;
+  }) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      name: data.get("name"),
+      price: data.get("price"),
+      time_window: data.get("time_window"),
+    });
+  };
+
   return (
-    <div>CreateItem</div>
-  )
-}
+    <Container component="main" maxWidth="lg">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{ mt: 1, width: "90%" }}
+        >
+          <Typography component="h1" variant="h5">
+            Create Item
+          </Typography>
 
-export default CreateItem
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="price"
+            label="Start Price"
+            type="text"
+            id="price"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="time_window"
+            label="Time Window"
+            type="text"
+            id="time_window"
+          />
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={2}
+            sx={{ mt: 1, mb: 2 }}
+          >
+            <Grid item>
+              <Button type="submit" variant="contained">
+                Create
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
--- auction-system-fe/src/pages/Deposit/Deposit.tsx
+++ auction-system-fe/src/pages/Deposit/Deposit.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-function Deposit() {
+export default function Deposit() {
+  const handleSubmit = (event: {
+    preventDefault: () => void;
+    currentTarget: HTMLFormElement | undefined;
+  }) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      deposit: data.get("deposit"),
+    });
+  };
+
   return (
-    <div>Deposit</div>
-  )
-}
+    <Container component="main" maxWidth="lg">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{ mt: 1, width: "90%" }}
+        >
+          <Typography component="h1" variant="h5">
+            Deposit
+          </Typography>
 
-export default Deposit
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="deposit"
+            label="Amount"
+            name="deposit"
+            autoFocus
+          />
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={2}
+            sx={{ mt: 1, mb: 2 }}
+          >
+            <Grid item>
+              <Button type="submit" variant="contained">
+                Deposit
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
